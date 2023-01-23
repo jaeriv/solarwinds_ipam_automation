e9b1a08c1797082d612e58729b316b80472f5d75
Jonathon Rivera <69470875+jaeriv@users.noreply.github.com>
2023-01-22 19:33:42 -0600

    Create change_status.py

---
+++ samples/change_status.py
@@ -0,0 +1,28 @@
+from orionsdk import SwisClient
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+username = ""
+password = ""
+server = ""
+ip_address = "x.x.x.x"
+
+def change_status():
+    """
+    Change status of IP in SolarWinds IPAM
+    1 = Used
+    2 = Available
+    4 = Reserved 
+    """
+    swis = SwisClient(server, username, password, verify=False)
+    results = swis.invoke(
+        "IPAM.SubnetManagement",
+        "ChangeIpStatus",
+        ip_address,
+        2 # Status Code
+    )
+    print(results)
+    return results
+
+# change_status()
